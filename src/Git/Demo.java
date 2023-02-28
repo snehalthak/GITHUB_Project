d53fe5a7bf13a990756db5a54a9a51d9c5169330
Snehal <Snehal@DESKTOP-N978OLJ>
2023-02-28 21:11:14 +0530

    First Commit..

---
+++ src/Git/Demo.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("Eat Something...");
+		}else
+		{
+			System.out.println("DO Work...");
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Snehal
+ *
+ */
+module GITHUB_Project {
+}
